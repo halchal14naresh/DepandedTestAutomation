07ffd97af2d75336c6d31d0aab27ce9ac29ed617
Naresh <halchal14naresh@gmail.com>
2022-04-11 21:13:34 +0530

    first commitreDOWNLOADED111ss

--- src/mytest1/Class1.java
+++ src/mytest1/Class1.java
@@ -1,11 +1,18 @@
 package mytest1;
 
+import mytest2.Base1;
+
+ 
+
  
 
 public class Class1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hiddaaaaq1 ");
+		Base1 obj = new Base1();
+		obj.add(2, 3);
+		
 	}
 
 }
